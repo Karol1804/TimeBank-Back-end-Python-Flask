f39edd70b5d8230ea5747f822aa29721181a9d47
Milan Juhas <thejustinian111@gmail.com>
2022-05-04 15:09:19 +0200

    Prepare for testing

--- timebank/__init__.py
+++ timebank/__init__.py
@@ -24,7 +24,11 @@ def before_first_request():
     for handler in app.logger.handlers:
         app.logger.removeHandler(handler)
 
-    log_file = os.path.join('app.log')
+    root = os.path.dirname(os.path.abspath(__file__))
+    logdir = os.path.join(root, 'logs')
+    if not os.path.exists(logdir):
+        os.mkdir(logdir)
+    log_file = os.path.join(logdir, 'app.log')
     handler = logging.FileHandler(log_file)
     handler.setLevel(log_level)
     app.logger.addHandler(handler)
