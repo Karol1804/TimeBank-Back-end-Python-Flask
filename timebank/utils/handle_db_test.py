8946cd1a763d4cc072529715fb1a572e72e4a2bb
Milan Juhas <thejustinian111@gmail.com>
2022-05-06 07:50:35 +0200

    Editing Config, handle_db and tests

--- timebank/utils/handle_db_test.py
+++ timebank/utils/handle_db_test.py
@@ -1,14 +1,11 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-       # host="127.0.0.1",  # Localhost
-       host="157.245.27.101",  # Testing
-       # host="157.230.79.85",  # Production
+       host="157.245.27.101",
        port='33306',
        user="automation",
-       password="ue1roo0uawechai5nieg1B",  # Testing
-       # password="Fej1chahgheebohxohxi",  # Production
-       database="timebank",
+       password="ue1roo0uawechai5nieg1B",
+       database="timebank_testing",
 )
 
 mycursor = mydb.cursor()
@@ -51,23 +48,3 @@ sql4 = "CREATE TABLE Serviceregister" \
        "rating INT);"
 
 mycursor.execute(sql4)
-
-sql5 = "INSERT INTO User (phone, user_name, time_account, password)" \
-       "VALUES ('+421 905 111222', 'Obi-wan Kenobi', '0', 'pbkdf2:sha256:" \
-       "260000$yogvVRpbV5xHD8VJ$7c4cc8e3f54988c81a175afad559b08b06ec1f35c7063898dba674929ce9f9d2');"
-
-sql6 = "INSERT INTO User (phone, user_name, time_account, password)"\
-       "VALUES ('+421 905 333444', 'Darth Vader', '0', 'pbkdf2:sha256:" \
-       "260000$FnLSMwPNR4A9quhV$cde29184e427e545ed22a4e8f3f5317f0d21060f2fd7d75b785acdf211e0d031');"
-
-sql7 = "INSERT INTO Service (title, user_id)" \
-       "VALUES ('Pokosim travnik', '1');"
-
-sql8 = "INSERT INTO Serviceregister (service_id, consumer_id,service_status)" \
-       "VALUES ('1', '2', 'inprogress');"
-
-mycursor.execute(sql5)
-mycursor.execute(sql6)
-mycursor.execute(sql7)
-mycursor.execute(sql8)
-mydb.commit()
