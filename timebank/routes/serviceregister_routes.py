9bf2ad89e100dbcaebe60036ce721df8c1986a1b
znamar00 <fFifVw3NYvr3>
2022-05-06 14:44:01 +0200

    removed "/" from serviceregister_routes serious programming happened

--- timebank/routes/serviceregister_routes.py
+++ timebank/routes/serviceregister_routes.py
@@ -223,7 +223,7 @@ def api_single_serviceregister_create():
     return api_single_registerservice_get(db_obj.id)
 
 
-@app.route('/api/v1/serviceregister/<serviceregister_id>/<hours>/', methods=['PUT'])
+@app.route('/api/v1/serviceregister/<serviceregister_id>/<hours>', methods=['PUT'])
 @app.route('/api/v1/serviceregister/<serviceregister_id>/<hours>/<rating>', methods=['PUT'])
 @jwt_required(optional=True)
 def api_single_serviceregister_finish_rating(serviceregister_id, hours, rating=None):
