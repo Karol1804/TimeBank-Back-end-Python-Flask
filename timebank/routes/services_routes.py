7bdbd41f16133646edf74fbf176e6bc5e50a33c7
mchovan <viktorijagrigojeva@gmail.com>
2022-05-09 14:00:48 +0200

    V service_routes pridane komentare.

--- timebank/routes/services_routes.py
+++ timebank/routes/services_routes.py
@@ -1,3 +1,4 @@
+# Import funkcii a modulov
 from flask import request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from sqlalchemy import text
@@ -9,16 +10,21 @@ from timebank.libs.response_helpers import record_sort_params_handler,\
     get_all_db_objects, is_number, ValidationError, is_estimate
 
 
+# Funckia na vytiahnutie vsetkych servisov z databazi
 @app.route('/api/v1/services', methods=['GET'])
 def api_services():
+
+    # Z requestu overujeme sortovacie parametre
     sort_field, sort_dir, valid = record_sort_params_handler(request.args, Service)
     if not valid:
         app.logger.error(f"{request.remote_addr}, Request in get all services failed, check your request and try again")
         return {'Bad Request': 'services not found'}, 400
 
+    # Do premennej db_objs pridelime vsetky servisy podla zadanych sortovacich parametrov
     db_objs = get_all_db_objects(sort_field, sort_dir, db.session.query(Service)).all()
     if len(db_objs):
         response_obj = []
+        # Iterujeme cez vsetky servisy
         for obj in db_objs:
             response_obj.append(dict(
                 id=obj.id,
@@ -39,9 +45,13 @@ def api_services():
         return '', 404
 
 
+# Funckia na vytiahnutie konkretneho servisu podla jeho id z databazi
 @app.route('/api/v1/service/<services_id>', methods=['GET'])
 def api_single_service_get(services_id):
+
+    # Vytiahnem z databazy model Service
     db_query = db.session.query(Service)
+    # Do premennej obj si ulozim konkretny servis podla servis_id
     obj = db_query.get(services_id)
 
     if not obj:
@@ -66,6 +76,7 @@ def api_single_service_get(services_id):
     return response, 200
 
 
+# Funkcia na updatovanie servisu z databazi
 @app.route('/api/v1/service/<services_id>', methods=['PUT'])
 @jwt_required(optional=True)
 def api_single_service_put(services_id):
@@ -87,11 +98,13 @@ def api_single_service_put(services_id):
         req_data = request.form
 
     if 'title' in req_data:
+        # Osetrenie velkosti titlu.. max 1000 znakov.
         if len(req_data['title']) > 1000:
             app.logger.warning(f"{request.remote_addr}, Title too long.")
             return jsonify({'error': 'Title too long.'}), 400
         db_obj.title = req_data['title']
 
+    # V podmienke validujeme estimate ci je cislo a ci je vacsie ako 0
     if 'estimate' in req_data:
         try:
             is_number(req_data['estimate'])
@@ -104,6 +117,7 @@ def api_single_service_put(services_id):
         db_obj.estimate = req_data['estimate']
 
     try:
+        # Prida zmenu do databazy
         db.session.commit()
         db.session.refresh(db_obj)
     except IntegrityError as e:
@@ -117,6 +131,7 @@ def api_single_service_put(services_id):
     return jsonify({'Message': 'Service successfully edited'}), 204
 
 
+# Funkcia na zmazanie servisu z databazi
 @app.route('/api/v1/service/<services_id>', methods=['DELETE'])
 @jwt_required(optional=True)
 def api_single_service_delete(services_id):
@@ -124,9 +139,12 @@ def api_single_service_delete(services_id):
         app.logger.warning(f"{request.remote_addr}, User is not logged in.")
         return jsonify({'error': 'User is not logged in.'}), 401
     db_query = db.session.query(Service)
+
+    # Do premennej db_test ulozime konkretny servis podla jeho ID
     db_test = db_query.get(services_id)
     db_obj = db_query.filter_by(id=services_id)
 
+    # Pokial taky servis neexistuje vyhodi nam error a funckia skonci
     if not db_test:
         app.logger.warning(f"{request.remote_addr}, Selected service: {services_id} does not exist.")
         return {'Bad Request': f'Service {services_id} not found'}, 400
@@ -145,9 +163,11 @@ def api_single_service_delete(services_id):
         return jsonify({'Message': 'Service successfully deleted'}), 204
 
 
+# Funckia na vytvorenie servisu a pridania ho do databazi
 @app.route('/api/v1/service-create', methods=['POST'])
 @jwt_required(optional=True)
 def api_single_service_create():
+    # Kontrola ci je uzivatel prihlaseny. Ak nie vrati error + koment "uzivatel nieje prihlaseny"
     if get_jwt_identity() is None:
         app.logger.warning(f"{request.remote_addr}, User is not logged in.")
         return jsonify({'error': 'User is not logged in.'}), 401
@@ -160,12 +180,14 @@ def api_single_service_create():
         req_data = request.form
 
     db_query2 = db.session.query(User)
+    # Overujeme ci uzivatel moze vytvorit servis tym ze kontrolujeme ci je cislo z jeho tokenu v databaze
     obj2 = db_query2.filter_by(phone=get_jwt_identity()).one()
 
+    # Overujeme ci je title v tele requestu. Ak nie vrati nam error.
     if 'title' not in req_data:
         app.logger.warning(f"{request.remote_addr}, Title is missing. Check your request and try again.")
         return jsonify({'error': 'Title is missing.'}), 400
-
+    # Overujeme dlhziu titlu. Ak ma viac ako 1000 znakov tak nam vrati error.
     if len(req_data['title']) > 1000:
         app.logger.warning(f"{request.remote_addr}, Title too long.")
         return jsonify({'error': 'Title too long.'}), 400
@@ -209,6 +231,7 @@ def api_service_search():
         app.logger.warning(f"{request.remote_addr}, Search of services failed, check your request and try again.")
         return '', 400
 
+    # Z URL hladame kluc[key] "s" a pridame mu jeho hodnotu["value"] ktoru vycitame z URL
     if request.args.get('s'):
         search_string = request.args.get('s')
     else:
@@ -216,12 +239,16 @@ def api_service_search():
         return '', 400
 
     response_obj = []
+    # Dlzka hladanej hodnoty musi mat minimalne 3 znaky a viac
     if len(search_string) > 2:
         db_query = db.session.query(Service)
+        # Zo servis titles filtrujeme vsetky titles ktore maju v sebe hladany string. To je hodnotu pri kluci "s" z URL
         db_filter = db_query.filter(Service.title.like('%' + search_string + '%'))
 
+        # Pokial sme zadali sortovacie parametre tak ich podla nich zoradime
         if field and sort_dir:
             db_objs = db_filter.order_by(text(field + ' ' + sort_dir)).all()
+        # Pokial sme nezadali sortovacie parametre tak ich zoradime zaradom ako idu.
         else:
             db_objs = db.filter.all()
 
